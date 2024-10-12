bdf897a3aafa94121d24a1dc19dd691ba4626926
nick <nick@nickaguila.me>
2024-10-11 20:42:54 -0400

    feat: auth provider added

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,7 @@ import { BrowserRouter as Router } from 'react-router-dom'
 import App from './App.tsx'
 import { DarkModeProvider } from './context/DarkModeContext.tsx'
 import './index.css'
+import { Auth0ProviderWithNavigate } from './layouts/Auth0ProviderWithNavigate.tsx'
 import store from './redux/store'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
@@ -13,7 +14,9 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
     <Provider store={store}>
       <DarkModeProvider>
         <React.StrictMode>
-          <App />
+          <Auth0ProviderWithNavigate>
+            <App />
+          </Auth0ProviderWithNavigate>
         </React.StrictMode>
       </DarkModeProvider>
     </Provider>
